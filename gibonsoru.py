51c3f9a8cc2da23a69ef143b1c577c7978d6d955
Lassanie <dgknbarbaros@gmail.com>
2023-01-03 23:05:20 +0300

    Add files via upload
    Tür sorusu modülü

---
+++ gibonsoru.py
@@ -0,0 +1,11 @@
+def gibonTurleri():
+    gibonTuru = input('O zaman en sevdiğin gibon türü ne?')
+    if gibonTuru == 'lar gibonu':
+        print('güzel')
+    elif gibonTuru == 'beyaz yanaklı gibon':
+        print('çok iyi')
+    elif gibonTuru == 'siamang':
+        print('o da bazlıdır hocam')
+    else:
+        print('O gibon türünü bilmiyorum hocam')
+
