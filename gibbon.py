c832035ee3eecc91949c210f8a86ab21fe3d73cf
Lassanie <dgknbarbaros@gmail.com>
2023-01-03 22:02:00 +0300

    Add files via upload

---
+++ gibbon.py
@@ -0,0 +1,15 @@
+gibbon = input('Gibonlar bazlı mıdır?')
+
+while gibbon == 'bazlıdır':
+    gibonTuru = input('O zaman en sevdiğin gibon türü ne?')
+
+    if gibonTuru == 'lar gibonu':
+        print('güzel')
+    elif gibonTuru == 'beyaz yanaklı gibon':
+        print('çok iyi')
+    elif gibonTuru == 'siamang':
+        print('o da bazlıdır hocam')
+    else:
+        print('O gibon türünü bilmiyorum hocam')
+
+
